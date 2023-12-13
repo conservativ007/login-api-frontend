1a7378b367c8fb9b953f6ae01381eddcf857195a
Maks <alisa3d2007@gmail.com>
2023-12-13 11:20:52 +0300

    feat: added button exit to Home.tsx

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import { useUser } from '../../store/store'
+import { useNavigate } from 'react-router-dom'
 
 export function Home() {
-	const { name } = useUser(state => state)
-	return <h1 style={{ textAlign: 'center' }}>Hi {name} you're logged in.</h1>
+	const { name, setUserLogged } = useUser(state => state)
+
+	const navigate = useNavigate()
+
+	const exitToStartPage = () => {
+		navigate('/')
+		setUserLogged(false)
+	}
+
+	return (
+		<div className='home'>
+			<h1 style={{ textAlign: 'center' }}>Hi {name} you're logged in.</h1>
+			<button onClick={exitToStartPage}>exit</button>
+		</div>
+	)
 }
