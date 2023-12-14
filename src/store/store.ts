21031ed9f49b4103032d5cbb5ba788f6a03d16b9
Maks <alisa3d2007@gmail.com>
2023-12-14 14:26:17 +0300

    feat: added console.log(token) in the store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -72,6 +72,8 @@ export const useUser = create<User>(set => ({
 			if (response.ok) {
 				set(state => ({ ...state, isUserLogged: true }))
 				set(state => ({ ...state, name: data.name }))
+
+				console.log(data)
 			}
 
 			if (!response.ok) {
